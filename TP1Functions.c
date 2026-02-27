e337eca977285c9f6c81d86fb7ff7734f7ac04e4
Carchilaa <caarchila2003@gmail.com>
2026-02-27 19:11:31 +0100

    Algo KP variables fixees

--- TP1Functions.c
+++ TP1Functions.c
@@ -270,12 +270,124 @@ int KP_VP(dataSet *dsptr){
 
 	int rval;
 	int greedy_solution = KP_greedy(dsptr);
-	int relax_solution = KP_LP(dsptr);
+	double relax_solution = KP_LP(dsptr);
 	int n = dsptr->n;
+  double *x = malloc(n * sizeof(double));
 
-	for(int j = 0; j < n; j++){
-		//dsptr->c[j] = abs(dsptr->c[j] - (()))
-	}
+  STR *str = (STR *)malloc(sizeof(STR) * n);
 
-	return rval;
+  for (int i = 0; i < n; i++) {
+    str[i].a = dsptr->a[i];
+    str[i].c = dsptr->c[i];
+    str[i].ratio = (double)dsptr->c[i] / dsptr->a[i];
+    str[i].index = i;
+  }
+
+  qsort(str, n, sizeof(STR), comparaison);
+
+  int p = -1;
+  int poids_courrant = 0;
+  int b = dsptr->b;
+
+  for(int j = 0; j<n; j++){
+    if(poids_courrant + str[j].a > b){
+      p = j;
+      break;
+    }
+    poids_courrant += str[j].a;
+  }
+
+  if(p == -1){
+    printf("Tous les objets rentrent dans le sac. Pas besoin de reduir");
+    free(str);
+    return greedy_solution;
+  }
+
+  double cp = str[p].c;
+  double ap = str[p].a;
+  double ratio_p = cp / ap;
+
+
+  double ecart = relax_solution - (double)greedy_solution;
+  int variables_fixees = 0;
+
+  int b_reduit = dsptr->b;
+  int profit_fixe = 0;
+
+  for (int i = 0; i < n; i++) {
+        x[i] = -1;
+    }
+
+    for (int j = 0; j < n; j++) {
+        // c_j barre : |cj - (cp/ap)*aj|
+        double c_barre = fabs(str[j].c - (ratio_p * str[j].a));
+
+        if (c_barre >= ecart) {
+            variables_fixees++;
+            int index_original = str[j].index;
+
+            if (j < p) {
+                // On force l'objet à 1 (on le met dans le sac)
+                x[index_original] = 1;
+                b_reduit = b_reduit - str[j].a;
+                profit_fixe = profit_fixe + str[j].c;
+            } else {
+                // On force l'objet à 0 (on le jette)
+                x[index_original] = 0;
+            }
+        }
+    }
+
+    printf("Nombre de variables fixees : %d sur %d\n", variables_fixees, n);
+
+    // 6. Créer l'instance réduite et lancer la Programmation Dynamique
+    int n_reduit = n - variables_fixees;
+    int resultat_final = profit_fixe;
+
+    // S'il reste des objets à évaluer et qu'il reste de la place
+    if (n_reduit > 0 && b_reduit > 0) {
+        
+        // Création du nouveau sac à dos réduit
+        dataSet dsptr_reduit;
+        dsptr_reduit.n = n_reduit;
+        dsptr_reduit.b = b_reduit;
+        dsptr_reduit.a = (int *)malloc(sizeof(int) * n_reduit);
+        dsptr_reduit.c = (int *)malloc(sizeof(int) * n_reduit);
+
+        int idx_reduit = 0;
+        // On remplit le sac réduit en parcourant les objets dans leur ordre d'origine
+        for (int i = 0; i < n; i++) {
+            if (x[i] == -1) { // Si l'objet n'a pas été fixé
+                dsptr_reduit.a[idx_reduit] = dsptr->a[i];
+                dsptr_reduit.c[idx_reduit] = dsptr->c[i];
+                idx_reduit++;
+            }
+        }
+
+        // 7. Résoudre le problème réduit avec KP_dynamic
+        printf("\n--- LANCEMENT DP SUR PROBLEME REDUIT ---\n");
+        printf("Objets restants : %d, Capacite restante : %d\n", n_reduit, b_reduit);
+        
+        int dp_result = KP_dynamic(&dsptr_reduit); // Étape 10 de l'algorithme du PDF
+        
+        // Le résultat final est la somme de ce qu'on a pré-rempli + ce que le DP a trouvé
+        resultat_final = profit_fixe + dp_result;
+
+        // On libère la mémoire de l'instance réduite
+        free(dsptr_reduit.a);
+        free(dsptr_reduit.c);
+        
+    } else {
+        // Soit le sac est plein (b_reduit <= 0), soit tous les objets ont été fixés (n_reduit == 0)
+        printf("Aucun objet supplementaire a evaluer (Capacite restante: %d, Objets restants: %d).\n", b_reduit, n_reduit);
+    }
+
+    printf("\nProfit des objets fixes d'avance : %d\n", profit_fixe);
+    printf("--- PROFIT TOTAL FINAL (Apres reduction) : %d ---\n\n", resultat_final);
+
+    // 8. Nettoyage de la mémoire allouée
+    free(x);
+    free(str);
+
+    return resultat_final;
 }
